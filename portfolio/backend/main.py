50ee769647a07d7871e22ecf020c449a9c72a1d3
Ainy Gupta <ainy@surginatal.com>
2026-06-27 11:22:17 +0530

    Update CORS

--- portfolio/backend/main.py
+++ portfolio/backend/main.py
@@ -8,8 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://new-portfolio.vercel.app",
-        "https://new-portfolio-chi-pearl.vercel.app"
+        "https://new-portfolio-imbcfzg8l-ainy-guptas-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
