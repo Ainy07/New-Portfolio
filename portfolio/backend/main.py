252acf158b1d625bd74c4f1c6d484af93da9fc6b
Ainy Gupta <ainy@surginatal.com>
2026-06-27 12:09:40 +0530

    Fix allow_origins

--- portfolio/backend/main.py
+++ portfolio/backend/main.py
@@ -6,13 +6,8 @@ app = FastAPI(title="Ainy Gupta Portfolio API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://new-portfolio-jwts.vercel.app",
-        "https://new-portfolio-jwts-git-main-ainy-guptas-projects.vercel.app",
-        "https://new-portfolio-jwts-jzwqsioc5-ainy-guptas-projects.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
