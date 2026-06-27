3782dfd946d5148bd9515a0818231dcfc37663fc
Ainy Gupta <ainy@surginatal.com>
2026-06-27 11:20:22 +0530

    add

--- portfolio/frontend/src/services/api.js
+++ portfolio/frontend/src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log("API URL:", API_BASE_URL);
+
 export const getProjects = async () => {
   const response = await axios.get(`${API_BASE_URL}/projects`);
   return response.data;
