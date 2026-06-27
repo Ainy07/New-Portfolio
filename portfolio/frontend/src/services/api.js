40748a93a089dc69960ffa19f5417625ef4b306c
Ainy Gupta <ainy@surginatal.com>
2026-06-27 11:51:03 +0530

    Fix API routes

--- portfolio/frontend/src/services/api.js
+++ portfolio/frontend/src/services/api.js
@@ -5,16 +5,16 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log("API URL:", API_BASE_URL);
 
 export const getProjects = async () => {
-  const response = await axios.get(`${API_BASE_URL}/projects`);
+  const response = await axios.get(`${API_BASE_URL}/projects/`);
   return response.data;
 };
 
 export const getSkills = async () => {
-  const response = await axios.get(`${API_BASE_URL}/skills`);
+  const response = await axios.get(`${API_BASE_URL}/skills/`);
   return response.data;
 };
 
 export const sendContactForm = async (formData) => {
-  const response = await axios.post(`${API_BASE_URL}/contact`, formData);
+  const response = await axios.post(`${API_BASE_URL}/contact/`, formData);
   return response.data;
 };
